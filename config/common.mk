d50dc61e8f30a3753c04f10f94e5555063382fca
Mazda-- <alex@dirtyunicorns.com>
2020-01-20 21:58:34 +0530

    Themes: Add DU themes [2/3]
    Change-Id: Ibd4dbd2b8015a7ef71c1cde5f3078964d3577891

--- config/common.mk
+++ config/common.mk
@@ -191,6 +191,9 @@ $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
 $(call inherit-product-if-exists, vendor/partner_gms/products/turbo.mk)
 $(call inherit-product-if-exists, vendor/gapps/config.mk)
 
+# Include Custom theme files
+include vendor/pa/themes/backgrounds/themes.mk
+
 # Do not preoptimize prebuilts when building GApps
 DONT_DEXPREOPT_PREBUILTS := true
 
