83927bcc27e6cf83142c1f99a88c6c81eefcfb11
Mazda-- <alex@dirtyunicorns.com>
2020-01-20 21:58:34 +0530

    Themes: Add DU themes [2/3]

--- config/common.mk
+++ config/common.mk
@@ -191,6 +191,9 @@ $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
 $(call inherit-product-if-exists, vendor/partner_gms/products/turbo.mk)
 $(call inherit-product-if-exists, vendor/gapps/config.mk)
 
+# Include Benedition theme files
+include vendor/pa/themes/backgrounds/themes.mk
+
 # Do not preoptimize prebuilts when building GApps
 DONT_DEXPREOPT_PREBUILTS := true
 
